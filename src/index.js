cc2cbb38ff9efd1130691e58aa57265cfa3d9e64
kiselevgleb <kiselevgleb@gmail.com>
2020-07-17 18:04:07 +0300

    edit package JSON

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import './css/task.css';
-import './js/app';
-import './js/Ticket';
-import './js/TicketFull';
-import './js/repo';
--- src/js/TicketFull.js
+++
@@ -1,9 +0,0 @@
-export default class TicketFull {
-  constructor(id, name, description, status, created) {
-    this.id = id;
-    this.description = description;
-    this.name = name;
-    this.status = status;
-    this.created = created;
-  }
-}
