518dfdfebc8d4adf23095f4865605d639d42245f
kiselevgleb <kiselevgleb@gmail.com>
2020-07-18 12:49:17 +0300

    edited repo url

---
+++ repo.js
@@ -0,0 +1,58 @@
+// const data = [];
+
+// export default data;
+export {
+  allTickets,
+  ticketById,
+  createTicket,
+  delById,
+};
+let h3 = document.querySelector('h3');
+
+function allTickets() {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://kiselevgleb.github.io/HelpDesk-backEnd/?method=allTickets', false);
+  xhr.send();
+  if (xhr.status !== 200) {
+    alert(`${xhr.status}: ${xhr.statusText}`);
+  } else {
+    h3.innerHTML = xhr.responseText;
+    return xhr.responseText;
+  }
+}
+
+function ticketById(id) {
+  const url = `https://kiselevgleb.github.io/HelpDesk-backEnd/?method=ticketById&id=${id}`;
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', url, false);
+  xhr.send();
+  if (xhr.status !== 200) {
+    alert(`${xhr.status}: ${xhr.statusText}`);
+  } else {
+    return xhr.responseText;
+  }
+}
+
+function delById(form) {
+  const url = 'https://kiselevgleb.github.io/HelpDesk-backEnd/?method=delById';
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', url, false);
+  xhr.send(form);
+  if (xhr.status !== 200) {
+    alert(`${xhr.status}: ${xhr.statusText}`);
+  } else {
+    return xhr.responseText;
+  }
+}
+
+function createTicket(formData) {
+  const url = 'https://kiselevgleb.github.io/HelpDesk-backEnd/?method=createTicket';
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', url, false);
+  xhr.send(formData);
+  if (xhr.status !== 200) {
+    alert(`${xhr.status}: ${xhr.statusText}`);
+  } else {
+    return xhr.responseText;
+  }
+}
